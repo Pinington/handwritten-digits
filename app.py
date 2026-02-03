82f5042ed94beca57e0d6f261ecd34c4135963d2
Pinington <georges5757c@gmail.com>
2026-02-03 22:13:44 +0100

    Added form to infer result

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', result="None")
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    pass
+    return render_template('index.html', result="None")
 
 if __name__=="__main__":
     app.run(debug=True)
