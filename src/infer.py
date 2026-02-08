05335ff2ed557af2b55c217eb0586f5aab1125a2
Pinington <georges5757c@gmail.com>
2026-02-08 20:55:48 +0100

    Infer.py back to normal predictions

--- src/infer.py
+++ src/infer.py
@@ -8,20 +8,6 @@ model = Net().to(device)
 model.load_state_dict(torch.load("data/models/mnist_cnn.pth", map_location=device))
 model.eval()
 
-# Dummy input matching your model input shape
-dummy_input = torch.randn(1, 1, 28, 28, device=device)
-
-# Export to ONNX
-torch.onnx.export(
-    model,
-    dummy_input,
-    "data/models/digit_model.onnx",    # output ONNX file
-    input_names=["input"],
-    output_names=["output"],
-    opset_version=17,
-    dynamic_axes={"input": {0: "batch"}, "output": {0: "batch"}}  # allows variable batch sizes
-)
-
 def predict(im_data):
     """
     im_data: torch.Tensor of shape [1, 1, 28, 28], normalized [0,1]
