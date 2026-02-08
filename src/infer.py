1b22fe4adc6f24a256d205ed909079a045176a79
Pinington <georges5757c@gmail.com>
2026-02-08 20:55:22 +0100

    Onnx added to make hosting work

--- src/infer.py
+++ src/infer.py
@@ -1,5 +1,5 @@
 import torch
-from .model import Net  # make sure model.py is in same folder or package
+from model import Net  # make sure model.py is in same folder or package
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -7,6 +7,20 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net().to(device)
 model.load_state_dict(torch.load("data/models/mnist_cnn.pth", map_location=device))
 model.eval()
+
+# Dummy input matching your model input shape
+dummy_input = torch.randn(1, 1, 28, 28, device=device)
+
+# Export to ONNX
+torch.onnx.export(
+    model,
+    dummy_input,
+    "data/models/digit_model.onnx",    # output ONNX file
+    input_names=["input"],
+    output_names=["output"],
+    opset_version=17,
+    dynamic_axes={"input": {0: "batch"}, "output": {0: "batch"}}  # allows variable batch sizes
+)
 
 def predict(im_data):
     """
