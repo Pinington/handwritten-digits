b5ceed592820cd6302620a412b2206c63fce3bcd
Pinington <georges5757c@gmail.com>
2026-02-18 16:28:38 +0100

    Retrained Onnx model

--- src/onnx_train.py
+++ src/onnx_train.py
@@ -1,8 +1,8 @@
 import torch
 from src.model import Net
 
-model = Net()
-model.load_state_dict(torch.load("data/models/mnist_cnn.pth", map_location="cpu"))
+model = Net(15)
+model.load_state_dict(torch.load("data/models/kaggle_cnn.pth", map_location="cpu"))
 model.eval()
 
 dummy_input = torch.randn(1, 1, 28, 28)
